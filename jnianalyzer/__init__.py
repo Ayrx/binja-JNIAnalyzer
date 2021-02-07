2f841388b0d35f3c0aa17471e9d55a6f72ec20d2
Terry Chia <terrycwk1994@gmail.com>
2021-02-07 16:16:50 +0800

    Initial work on statically locating RegisterNatives calls.

--- jnianalyzer/__init__.py
+++ jnianalyzer/__init__.py
@@ -4,6 +4,7 @@ from binaryninja.interaction import get_open_filename_input
 from binaryninja.log import log_info, log_error
 from binaryninja.types import Type, Symbol
 from binaryninja.typelibrary import TypeLibrary
+from binaryninja.highlevelil import HighLevelILOperation
 from androguard.misc import AnalyzeAPK
 from collections import namedtuple
 import json
@@ -98,6 +99,82 @@ def apply_comment(func, method):
         )
 
 
+def set_registernatives(
+    bv, jnianalyzer_tagtype, class_name, methods_ptr, methods_count
+):
+    class_name_array = "{}_METHODS_ARRAY".format(class_name)
+
+    # Set JNINativeMethod type
+    t = bv.get_type_by_name("JNINativeMethod")
+    t_size = t.width
+    t = Type.array(t, methods_count)
+    bv.define_user_data_var(methods_ptr, t)
+
+    # Set function signature
+    for i in range(0, methods_count):
+        ptr = methods_ptr + (i * t_size)
+        data = StructuredDataView(bv, "JNINativeMethod", ptr)
+
+        method_name = str(bv.get_ascii_string_at(data.name.int, 1))
+        fn_ptr = data.fnPtr.int
+        signature = str(bv.get_ascii_string_at(data.signature.int, 1))
+
+        method = Method(
+            class_name,
+            method_name,
+            signature,
+            False,
+        )
+
+        f = bv.get_function_at(fn_ptr)
+        attr = str(f.function_type).split(")")[1]
+        log_info("Setting type for: {}".format(f.name))
+        f.function_type = build_binja_type_signature(f.name, method, attr)
+        apply_function_tag(f, jnianalyzer_tagtype, f.name)
+        apply_comment(f, method)
+
+    # Set symbol for array
+    sym = Symbol("DataSymbol", methods_ptr, class_name_array)
+    bv.define_user_symbol(sym)
+
+    # Set tag
+    apply_data_tag(bv, methods_ptr, jnianalyzer_tagtype, class_name_array)
+
+
+def hlil_check_jnienv_call(ins, offset):
+    """Returns True if a HLIL instruction is a call to a JNIEnv* function."""
+    if not ins.operation == HighLevelILOperation.HLIL_CALL:
+        return False
+
+    ops = ins.operands
+    callee = ops[0]
+
+    return (
+        callee.operation == HighLevelILOperation.HLIL_DEREF_FIELD
+        and str(callee.operands[0].expr_type) == "struct JNINativeInterface_*"
+        and callee.operands[2] == offset
+    )
+
+
+def process_findclass_call(bv, ins):
+    """Process a FindClass JNI function call and returns the class name.
+
+    This method only works if the class name is a constant within the binary
+    and not determined during runtime.
+    """
+    # 6 == FindClass
+    if hlil_check_jnienv_call(ins, 6):
+        ops = ins.operands
+        callee_args = ops[1]
+
+        if callee_args[1].operation == HighLevelILOperation.HLIL_CONST_PTR:
+            return bv.get_ascii_string_at(callee_args[1].value.value, 1)
+        else:
+            print(callee_args[1])
+
+    return None
+
+
 def import_apk(bv):
     jnianalyzer_tagtype = init_binja(bv)
 
@@ -147,46 +224,33 @@ def import_trace_registernatives(bv):
             methods_ptr = i["methods_ptr"]
             methods_count = i["nMethods"]
 
-            methods_ptr_int = int(methods_ptr, 16)
-            class_name_array = "{}_METHODS_ARRAY".format(class_name)
-
             log_info("Setting JNINativeMethod type at {}".format(methods_ptr))
+            set_registernatives(
+                bv, jnianalyzer_tagtype, class_name, int(methods_ptr, 16), methods_count
+            )
 
-            # Set JNINativeMethod type
-            t = bv.get_type_by_name("JNINativeMethod")
-            t_size = t.width
-            t = Type.array(t, methods_count)
-            bv.define_user_data_var(methods_ptr_int, t)
 
-            # Set function signature
-            for i in range(0, methods_count):
-                ptr = methods_ptr_int + (i * t_size)
-                data = StructuredDataView(bv, "JNINativeMethod", ptr)
+def locate_registernatives(bv):
+    jnianalyzer_tagtype = init_binja(bv)
 
-                method_name = str(bv.get_ascii_string_at(data.name.int, 1))
-                fn_ptr = data.fnPtr.int
-                signature = str(bv.get_ascii_string_at(data.signature.int, 1))
+    for func in bv.functions:
+        hlil = func.hlil
 
-                method = Method(
-                    class_name,
-                    method_name,
-                    signature,
-                    False,
+        for ins in hlil.instructions:
+            # 215 == RegisterNatives
+            if hlil_check_jnienv_call(ins, 215):
+                log_info("Found RegisterNatives call in: {}".format(func.name))
+                ops = ins.operands
+                callee_args = ops[1]
+                # class_name = i["name"]
+                class_name = process_findclass_call(bv, callee_args[1])
+                methods_ptr = callee_args[2].value.value
+                methods_count = callee_args[3].value.value
+
+                log_info("Setting JNINativeMethod type at {}".format(hex(methods_ptr)))
+                set_registernatives(
+                    bv, jnianalyzer_tagtype, class_name, methods_ptr, methods_count
                 )
-
-                f = bv.get_function_at(fn_ptr)
-                attr = str(f.function_type).split(")")[1]
-                log_info("Setting type for: {}".format(f.name))
-                f.function_type = build_binja_type_signature(f.name, method, attr)
-                apply_function_tag(f, jnianalyzer_tagtype, f.name)
-                apply_comment(f, method)
-
-            # Set symbol for array
-            sym = Symbol("DataSymbol", methods_ptr_int, class_name_array)
-            bv.define_user_symbol(sym)
-
-            # Set tag
-            apply_data_tag(bv, methods_ptr_int, jnianalyzer_tagtype, class_name_array)
 
 
 PluginCommand.register(
@@ -199,4 +263,10 @@ PluginCommand.register(
     "JNIAnalyzer: Import trace_registernatives JSON",
     "Import results from trace_registernatives output.",
     import_trace_registernatives,
+)
+
+PluginCommand.register(
+    "JNIAnalyzer: Locate RegisterNatives calls",
+    "Find RegisterNatives calls through HLIL analysis.",
+    locate_registernatives,
 )
